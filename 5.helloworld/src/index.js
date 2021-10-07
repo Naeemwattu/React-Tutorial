4e18fa0c930d4a3779ee92f4770f7c3e2ea4a4b6
Naeemwattu <naeemwattu6@gmail.com>
2021-10-07 13:06:06 +0500

    render multiple element

--- 5.helloworld/src/index.js
+++ 5.helloworld/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { createElement } from "react";
 // var React = require("react");
 
 import ReactDOM from "react-dom"
 // var ReactDOM = require("react-dom");
 
-// ReactDOM.render(<h1> Hello World!!! </h1>, document.getElementById('root'));
+ReactDOM.render(<h1> Hello World!!! </h1>, document.getElementById('root'));
 
 
 // BABEL CONVERT THE CODE
@@ -21,3 +20,26 @@ ReactDOM.render(React.createElement("h1",
 var h1 = document.createElement('h1');
 h1.innerHTML = "NOMIWATTU FRONTEND DEVELOPER";
 document.getElementById('root').appendChild(h1);
+
+
+
+
+
+//6.. How to Render Multiple Elements inside ReactDOM.render() in ReactJS
+// In React v16 its possible for render()
+// to return an array of an element
+
+ReactDOM.render(
+  <div>
+  <h1> Hello Dear!!! </h1> 
+  <p>My name is MUHAMMAD NAEEM ABBAS</p>
+  </div>
+  , document.getElementById('root'));
+
+  ReactDOM.render(
+    [
+      <h1> Hello World!!! </h1>, 
+      <p>My name is MUHAMMAD NAEEM ABBAS</p>,
+      <h2>I live in Sambrial Sialkot Mohallah Rasool pura Street no.4</h2>
+    ]
+    , document.getElementById('root'));
