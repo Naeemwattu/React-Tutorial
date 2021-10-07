fe2ae59187a2128b842ab63ea15f4d56694c07b1
Naeemwattu <naeemwattu6@gmail.com>
2021-10-07 12:31:40 +0500

    JSX CONCEPT

--- 5.helloworld/src/index.js
+++ 5.helloworld/src/index.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { createElement } from "react";
 // var React = require("react");
 
 import ReactDOM from "react-dom"
 // var ReactDOM = require("react-dom");
 
-ReactDOM.render(<h1> Hello World!!! </h1>, document.getElementById('root'));
+// ReactDOM.render(<h1> Hello World!!! </h1>, document.getElementById('root'));
+
+
+// BABEL CONVERT THE CODE
+
+ReactDOM.render(React.createElement("h1", 
+  null, 
+  " Hello World!!! "), 
+  document.getElementById('root'));
+
+
+// PURE JAVASCRIPT CODE
+
+var h1 = document.createElement('h1');
+h1.innerHTML = "NOMIWATTU FRONTEND DEVELOPER";
+document.getElementById('root').appendChild(h1);
