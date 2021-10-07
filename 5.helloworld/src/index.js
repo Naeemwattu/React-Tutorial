11c789c13a5e5b5f0b0607df9973ba051c4b5771
Naeemwattu <naeemwattu6@gmail.com>
2021-10-07 11:45:42 +0500

    helloworld

---
+++ 5.helloworld/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+// var React = require("react");
+
+import ReactDOM from "react-dom"
+// var ReactDOM = require("react-dom");
+
+ReactDOM.render(<h1> Hello World!!! </h1>, document.getElementById('root'));
