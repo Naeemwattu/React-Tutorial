7939aa1d42b5d5a0840a7b7074765c31e36629c9
Naeemwattu <naeemwattu6@gmail.com>
2021-10-13 23:02:57 +0500

    netflix app

---
+++ netflix/src/Cards.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Card(props) {
+    console.log(props)
+    return (
+        <>
+            <div className="cards">
+                <div className="card">
+                    <img src={props.imgsrc} alt="alt-imges" className="card-img" />
+                    <div className="card-info">
+                        <span className="card-title">{props.title}</span>
+                        <h3 className="card-category">{props.sname}</h3>
+                        <a href={props.link} target="_blank">
+                            <button>Watch Now</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Card;
---
+++ netflix/src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards";
+import "./index.css"
+
+
+
+ReactDOM.render(
+  <>
+    <Card 
+      imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTSzYjEvdK8w7VcJNIEsVLXUcjeqwKtiQEWw&usqp=CAU"
+      title="A Netflix Original Series"
+      sname="Dark Desire"
+      link="https://www.netflix.com/pk/title/81090319"
+    />
+    <Card 
+      imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrRPhWIEn96NIueUWb2AjZuo0vynP517J_hg&usqp=CAU"
+      title="A Netflix Original Series"
+      sname="Money Hiest"
+      link="https://www.netflix.com/title/80192098"
+    />
+    <Card 
+      imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVSGbbVGcyG-YfhIhYGaji-rpRrI0XNq44mQ&usqp=CAU"
+      title="A Netflix Original Series"
+      sname="Avengers End Game"
+      link="https://ew.com/movies/2019/11/06/avengers-endgame-streaming-disney-plus-launch-date/#:~:text=Unlike%20many%20recent%20movies%20from,%2C%20Disney%2B%2C%20and%20stay%20there."
+    />
+  </>,
+  document.getElementById('root')
+);
