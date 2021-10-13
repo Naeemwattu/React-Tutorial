8cba8f05870ff0da41f6fc0c208625fa9a3f1e5d
Naeemwattu <naeemwattu6@gmail.com>
2021-10-13 23:22:22 +0500

    Netflix Array Card

--- netflix/src/index.js
+++ netflix/src/index.js
@@ -1,29 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./Cards";
-import "./index.css"
+import "./index.css";
+import ArrayCard from "./ArrayCard";
 
 
 
 ReactDOM.render(
   <>
     <Card 
-      imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTSzYjEvdK8w7VcJNIEsVLXUcjeqwKtiQEWw&usqp=CAU"
-      title="A Netflix Original Series"
-      sname="Dark Desire"
-      link="https://www.netflix.com/pk/title/81090319"
+      imgsrc={ArrayCard[0].imgsrc}
+      title={ArrayCard[0].title}
+      sname={ArrayCard[0].sname}
+      link={ArrayCard[0].link}
     />
     <Card 
-      imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrRPhWIEn96NIueUWb2AjZuo0vynP517J_hg&usqp=CAU"
-      title="A Netflix Original Series"
-      sname="Money Hiest"
-      link="https://www.netflix.com/title/80192098"
+      imgsrc={ArrayCard[1].imgsrc}
+      title={ArrayCard[1].title}
+      sname={ArrayCard[1].sname}
+      link={ArrayCard[1].link}
     />
     <Card 
-      imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVSGbbVGcyG-YfhIhYGaji-rpRrI0XNq44mQ&usqp=CAU"
-      title="A Netflix Original Series"
-      sname="Avengers End Game"
-      link="https://ew.com/movies/2019/11/06/avengers-endgame-streaming-disney-plus-launch-date/#:~:text=Unlike%20many%20recent%20movies%20from,%2C%20Disney%2B%2C%20and%20stay%20there."
+      imgsrc={ArrayCard[2].imgsrc}
+      title={ArrayCard[2].title}
+      sname={ArrayCard[2].sname}
+      link={ArrayCard[2].link}
+    />
+    <Card 
+      imgsrc={ArrayCard[3].imgsrc}
+      title={ArrayCard[3].title}
+      sname={ArrayCard[3].sname}
+      link={ArrayCard[3].link}
+    />
+    <Card 
+      imgsrc={ArrayCard[4].imgsrc}
+      title={ArrayCard[4].title}
+      sname={ArrayCard[4].sname}
+      link={ArrayCard[4].link}
     />
   </>,
   document.getElementById('root')
