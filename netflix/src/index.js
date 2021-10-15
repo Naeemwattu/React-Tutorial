240c22e0830d7ec465538aafb7b79c1b3914e643
Naeemwattu <naeemwattu6@gmail.com>
2021-10-16 00:16:48 +0500

    update arrow function

--- netflix/src/index.js
+++ netflix/src/index.js
@@ -4,24 +4,36 @@ import Card from "./Cards";
 import "./index.css";
 import ArrayCard from "./ArrayCard";
 
-function ncard(val){
-  console.log(val)
-  return(
-    <>
-      <Card
-      imgsrc={val.imgsrc}
-      title={val.title}
-      sname={val.sname}
-      link={val.link}
-    />
-    </>
-  );
-}
+// function ncard(val) {
+//   console.log(val)
+// return (
+//   <>
+//     <Card
+//       imgsrc={val.imgsrc}
+//       title={val.title}
+//       sname={val.sname}
+//       link={val.link}
+//     />
+//   </>
+//   );
+// }
 
 ReactDOM.render(
   <>
     <h1 className="heading">Top 5 Netflix Series with Links</h1>
-    {ArrayCard.map(ncard)}
+    {/* {ArrayCard.map(ncard)} */}
+    {ArrayCard.map((val) =>  {
+      return (
+        <>
+          <Card
+            imgsrc={val.imgsrc}
+            title={val.title}
+            sname={val.sname}
+            link={val.link}
+          />
+        </>
+      );
+    })}
   </>,
   document.getElementById('root')
 );
