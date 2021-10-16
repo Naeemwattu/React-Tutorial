7c1c43a4b9d4ae6888878c04d36b0516a0ef6cbc
Naeemwattu <naeemwattu6@gmail.com>
2021-10-16 23:10:36 +0500

    Netflix cmplt

--- netflix/src/ArrayCard.jsx
+++ netflix/src/ArrayCard.jsx
@@ -1,41 +1,48 @@
 const ArrayCard = [
     {
+        id :  1,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdQYedlm-NJDVCfMhhqqPHmjew70qV9PU1XQ&usqp=CAU",
         title: "A Netflix Original Series",
         sname: "Squad Game",
         link: "https://www.netflix.com/pk/title/81040344?source=35",
     },
     {
+        id:  2,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc5S2NAUF6eVeBwpiVEvAny2g56uqn-hG82g&usqp=CAU",
         title: "A Netflix Original Series",
         sname: "Money Hiest",
         link: "https://www.netflix.com/title/80192098",
     },
     {
+        id:  3,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0vMcSYGarxLE5jgyVtMF_18bP2s8o0uc3HQ&usqp=CAU",
         title: "A Netflix Original Series",
         sname: "Lucifer",
         link: "https://www.netflix.com/title/80057918",
     },
     {
+        id:  4,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL9VQAD1XeA-r13zif_6WxH_HbcYyiKyg7Ig&usqp=CAU",
         title: "A Netflix Original Series",
         sname: "Stranger Things",
         link: "https://www.netflix.com/title/80057281",
     },
     {
+        id:  5,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYf8JmsYMCXQOg6eWsa4C9Coyk7ImYcBExZA&usqp=CAU",
         title: "A Netflix Original Series",
         sname: "Dark Desire",
         link: "https://www.netflix.com/title/81090319",
     },
     {
+        id:  6,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStkuZAaWBJTnKmazmzJl8D0TyRB81Lb9_DoQ&usqp=CAU",
         title: "A Netflix Original Series",
         sname: "Pekay Blinders",
         link: "https://www.netflix.com/title/80002479",
     },
     {
+        id: 7,
         imgsrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc5S2NAUF6eVeBwpiVEvAny2g56uqn-hG82g&usqp=CAU",
         title: "A Netflix Original Series",
         sname: "Money Hiest",
--- netflix/src/index.js
+++ netflix/src/index.js
@@ -1,39 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./Cards";
 import "./index.css";
-import ArrayCard from "./ArrayCard";
+import App from "./App";
 
-// function ncard(val) {
-//   console.log(val)
-// return (
-//   <>
-//     <Card
-//       imgsrc={val.imgsrc}
-//       title={val.title}
-//       sname={val.sname}
-//       link={val.link}
-//     />
-//   </>
-//   );
-// }
-
-ReactDOM.render(
-  <>
-    <h1 className="heading">Top 5 Netflix Series with Links</h1>
-    {/* {ArrayCard.map(ncard)} */}
-    {ArrayCard.map((val) =>  {
-      return (
-        <>
-          <Card
-            imgsrc={val.imgsrc}
-            title={val.title}
-            sname={val.sname}
-            link={val.link}
-          />
-        </>
-      );
-    })}
-  </>,
+ReactDOM.render(<App/>,
   document.getElementById('root')
 );
