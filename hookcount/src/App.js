b6e42891aa473f6668fe1fffbfd399160953eb97
Naeemwattu <naeemwattu6@gmail.com>
2021-10-30 13:10:45 +0500

    Events Handling in HOOKS React.js

--- hookcount/src/App.js
+++ hookcount/src/App.js
@@ -21,33 +21,62 @@
 // export default App;
 
 
+// import React, { useState } from "react";
+// const App = () => {
+
+//   let time = new Date().toLocaleTimeString();
+//   const [ctime, newCtime] = useState(time);
+
+//   const UpdateTime = () => {
+//     time = new Date().toLocaleTimeString();
+//     newCtime(time);
+//   }
+
+//   let date = new Date().toLocaleDateString();
+//   const [cdate, newCdate] = useState(date);
+
+//   const UpdateDate = ()=>{
+//     date = new Date().toLocaleDateString();
+//     newCdate(date);
+//   }
+//   setInterval(UpdateTime,1000);
+//   return (
+//     <>
+//       <div>
+//         <h1>{ctime}</h1>
+//         <h5>{cdate}</h5>
+//       </div>
+//     </>
+//   )
+// };
+// export default App;
+
 import React, { useState } from "react";
-const App = () => {
 
-  let time = new Date().toLocaleTimeString();
-  const [ctime, newCtime] = useState(time);
+const App = () =>{
 
-  const UpdateTime = () => {
-    time = new Date().toLocaleTimeString();
-    newCtime(time);
-  }
+  const color = "#0066ff";
+  const [bg, newBg] = useState(color);
+  const [name, setName] = useState('Yesterday PAK vs AFG Match Result Here... ✅')
 
-  let date = new Date().toLocaleDateString();
-  const [cdate, newCdate] = useState(date);
+  const setBg = () =>{
+    let newcolor = "#009933";
+    newBg(newcolor);
+    setName("Hurrah!! Pakistan Win the Match against Afghanistan 🥇 🏆")
 
-  const UpdateDate = ()=>{
-    date = new Date().toLocaleDateString();
-    newCdate(date);
-  }
-  setInterval(UpdateTime,1000);
-  return (
+  };
+
+  const setNewbg = () =>{
+    newBg(color);
+    setName("Boht Boht Mubarak ho pakistan Jeet gya ha.. ❤️  🥰");
+  };
+  return(
     <>
-      <div>
-        <h1>{ctime}</h1>
-        <h5>{cdate}</h5>
-        {/* <button onClick={UpdateTime}>Get Time</button> */}
+      <div style={{backgroundColor: bg}}>
+        <button onClick={setBg} onDoubleClick={setNewbg}> {name} </button>
       </div>
     </>
   )
 };
+
 export default App;
