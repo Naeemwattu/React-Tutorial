9aa916a31f82d257eb9397be74ca6727ddd9243d
Naeemwattu <naeemwattu6@gmail.com>
2021-11-03 20:21:54 +0500

    spread function

--- formreact/src/App.js
+++ formreact/src/App.js
@@ -1,23 +1,113 @@
 import React, { useState } from "react";
 
-const App =()=>{
-  const [name, setName] = useState();
-  const [fullName, setFullName] = useState();
+const App = () => {
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    number: "",
+    qua: "",
+  });
 
-  const inputEvent=(event)=>{
+
+  const inputEvent = (event) => {
     console.log(event.target.value);
-    setName(event.target.value);
+    console.log(event.target.name);
+
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setFullName((preValue) => {
+      return{
+        ...preValue,
+        [name] : value,
+      }
+
+
+      // if (name === "fName") {
+      //   return {
+      //     fname: value,
+      //     lname: preValue.lname,
+      //     email: preValue.email,
+      //     number: preValue.number,
+      //   };
+      // } else if (name === "lName") {
+      //   return {
+      //     fname: preValue.fname,
+      //     lname: value,
+      //     email: preValue.email,
+      //     number: preValue.number,
+      //   };
+      // }else if (name === "email") {
+      //   return {
+      //     fname: preValue.fname,
+      //     lname: preValue.lname,
+      //     email: value,
+      //     number: preValue.number,
+      //   };
+      // }else if (name === "number") {
+      //   return {
+      //     fname: preValue.fname,
+      //     lname: preValue.lname,
+      //     email: preValue.email,
+      //     number: value,
+      //   };
+      // }
+
+    });
   }
 
-  const onSubmit =()=>{
-    setFullName(name);
+  const onSubmits = (event) => {
+    event.preventDefault();
+    alert("Form Submitted successfully");
   }
-  return(
+
+  return (
     <>
-      <div>
-        <h1>Hello {fullName} </h1>
-        <input type="text" placeholder="Enter Your Name" onChange={inputEvent} value={name} />
-        <button onClick={onSubmit} >Submit</button>
+      <div className="main_div">
+        <form onSubmit={onSubmits} >
+          <div>
+
+            <h1>Hello {fullName.fname} {fullName.lname}</h1>
+            <p> {fullName.email} </p>
+            <p> {fullName.number} </p>
+            <p> {fullName.qua} </p>
+
+
+            <input type="text"
+              placeholder="Enter Your Name"
+              name="fname"
+              onChange={inputEvent}
+              value={fullName.fname} />
+
+            <input type="text"
+              placeholder="Enter Your last Name"
+              name="lname"
+              onChange={inputEvent}
+              value={fullName.lname} />
+
+            <input type="email"
+              placeholder="Enter Your Email"
+              name="email"
+              onChange={inputEvent}
+              value={fullName.email} />
+
+            <input type="number"
+              placeholder="Enter Your Phone Number"
+              name="number"
+              onChange={inputEvent}
+              value={fullName.number} />
+
+              <input type="text"
+              placeholder="Enter Your Qualification"
+              name="qua"
+              onChange={inputEvent}
+              value={fullName.qua} />
+
+            <button type="submit">Submit</button>
+
+          </div>
+        </form>
       </div>
     </>
   )
